d86705e4584ffa421e2f09a422239a292e49d994
jhaeme15 <jaredhaeme15@augustana.edu>
2019-03-25 19:22:51 -0500

    Adding indiv project to github

--- app/src/main/java/com/example/fitnesstrackertest/SetsPage.java
+++ app/src/main/java/com/example/fitnesstrackertest/SetsPage.java
@@ -5,6 +5,7 @@ import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -12,13 +13,35 @@ import android.widget.TextView;
 
 public class SetsPage extends AppCompatActivity {
     private LinearLayout linearLayout;
+    private Button btnAddSet;
+    private Button btnDeleteLift;
+    private RelativeLayout btnLayout;
+    private RelativeLayout relativeLayout;
+    private EditText editTextWeight;
+    private EditText editTextRep;
+    private TextView xdivider;
+    private TextView setNumlbl;
+    private EditText liftType;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sets_page);
         Lift lift = (Lift) getIntent().getSerializableExtra("choosenLift");
+        liftType=(EditText) findViewById(R.id.editTxtSetsPageLiftType) ;
+
         linearLayout=(LinearLayout) findViewById(R.id.lin_layout_setsPage);
-        RelativeLayout relativeLayout=new RelativeLayout(this);
+        if(lift!=null){
+            liftType.setText(lift.getLift());
+
+        }
+        createSetUI(linearLayout);
+        createButtonUI(linearLayout);
+    }
+
+    public void createSetUI(LinearLayout linearLayout){
+        relativeLayout=new RelativeLayout(this);
         RelativeLayout.LayoutParams params=new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         RelativeLayout.LayoutParams edittxtWeightParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         params.setMargins(0,5,0,0);
@@ -26,14 +49,14 @@ public class SetsPage extends AppCompatActivity {
         RelativeLayout.LayoutParams edittxtRepParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         RelativeLayout.LayoutParams lblxParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         RelativeLayout.LayoutParams lblSetsParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-        EditText editTextWeight=new EditText(this);
-        editTextWeight.setId(View.generateViewId());
 
-        EditText editTextRep=new EditText(this);
+        editTextWeight=new EditText(this);
+        editTextWeight.setId(View.generateViewId());
+        editTextRep=new EditText(this);
         editTextRep.setId(View.generateViewId());
-        TextView xdivider=new TextView(this);
+        xdivider=new TextView(this);
         xdivider.setId(View.generateViewId());
-        TextView setNumlbl=new TextView(this);
+        setNumlbl=new TextView(this);
         setNumlbl.setId(View.generateViewId());
 
         editTextWeight.setHint("Weight");
@@ -72,9 +95,33 @@ public class SetsPage extends AppCompatActivity {
         View divider=new View(this);
         divider.setLayoutParams(new LinearLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, 1));
         divider.setBackgroundColor(Color.parseColor("#B3B3B3"));
-
         linearLayout.addView(relativeLayout, params);
         linearLayout.addView(divider);
-
     }
+
+    public void createButtonUI(LinearLayout linearLayout){
+        btnAddSet =new Button(this);
+        btnDeleteLift=new Button(this);
+        btnAddSet.setText("Add Set");
+        btnDeleteLift.setText("Delete Lift");
+        btnAddSet.setId(View.generateViewId());
+        btnDeleteLift.setId(View.generateViewId());
+        btnLayout=new RelativeLayout(this);
+        RelativeLayout.LayoutParams btnRelLayoutparams=new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        btnRelLayoutparams.setMargins(0,5,0,0);
+        btnLayout.setLayoutParams(btnRelLayoutparams);
+        RelativeLayout.LayoutParams btnAddSetParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        RelativeLayout.LayoutParams btnDeleteLiftParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        btnAddSetParams.setMargins(30, 0, 10, 0);
+        btnDeleteLiftParams.setMargins(10, 0, 30, 0);
+        btnAddSetParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+        btnDeleteLiftParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        btnAddSet.setLayoutParams(btnAddSetParams);
+        btnDeleteLift.setLayoutParams(btnDeleteLiftParams);
+        btnLayout.addView(btnAddSet, btnAddSetParams);
+        btnLayout.addView(btnDeleteLift, btnDeleteLiftParams);
+
+        linearLayout.addView(btnLayout, btnRelLayoutparams);
+    }
+
 }
